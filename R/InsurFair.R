96663af756d87d6810d62d7f29fc1063548ba671
Arthur Charpentier <arthur.charpentier@gmail.com>
2023-02-13 10:39:19 -0500

    Add files via upload

---
+++ R/InsurFair.R
@@ -0,0 +1,11 @@
+#' InsurFair Package
+#'
+#' ....
+#'
+#'
+#' @docType package
+#'
+#' @author Arthur Charpentier \email{arthur.charpentier@gmail.com}
+#'
+#' @name package1
+NULL
---
+++ R/toy1.R
@@ -0,0 +1,12 @@
+#' Toy1 Dataset
+#'
+#' Simple toy dataset with 20 observations
+#'
+#'
+#' @examples
+#' data(toy1)
+#' str(toy1)
+#'
+#' @export
+toy1 = data.frame(x = runif(20),
+                  y = runif(20))
